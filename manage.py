8a7e7f46825e44ab06b7d5451921189599c42c78
Lavilley <lavilleyvincent@gmail.com>
2023-07-26 17:56:23 +0200

    fixtures, data cleaning

--- manage.py
+++ manage.py
@@ -2,7 +2,6 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from projet_AFPA.get_data import load_db
 
 def main():
     """Run administrative tasks."""
@@ -15,7 +14,6 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    load_db()
     execute_from_command_line(sys.argv)
     
 
--- projet_AFPA/get_data.py
+++ projet_AFPA/get_data.py
@@ -1,18 +1,40 @@
 import requests
 import json
 import os
+from datetime import datetime
 
 base_url = "https://api-football-v1.p.rapidapi.com/v3/"
 headers = {
 	"X-RapidAPI-Key": "75a37ef9a9msh7007ea31089e9a2p1b8d8bjsn6f08f8c5b5ed",
 	"X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
 }
+
+liste_clubs = {'AS MONACO': 'Monaco',
+ 'CLERMONT FOOT 63': 'Clermont-Ferrand',
+ 'FC LORIENT': 'Lorient',
+ 'FC METZ': 'Metz',
+ 'FC NANTES': 'Nantes',
+ 'HAVRE AC': 'Le Havre',
+ 'LOSC LILLE': 'Lille',
+ 'MONTPELLIER HERAULT SC': 'Montpellier',
+ 'OGC NICE': 'Nice',
+ 'OLYMPIQUE DE MARSEILLE': 'Marseille',
+ 'OLYMPIQUE LYONNAIS': 'Lyon',
+ 'PARIS SAINT-GERMAIN': 'Paris',
+ 'RC LENS': 'Lens',
+ 'RC STRASBOURG ALSACE': 'Strasbourg',
+ 'STADE BRESTOIS 29': 'Brest',
+ 'STADE DE REIMS': 'Reims',
+ 'STADE RENNAIS FC': 'Rennes',
+ 'TOULOUSE FC': 'Toulouse'}
+
+
 # Chemin d'accès aux données en format json
 dir_path = os.path.dirname(os.path.realpath(__file__))
 json_path = os.path.dirname(dir_path) + "/data/"
     
 
-def get_ligue1_journees():
+def get_journees():
     #Récupère les données des journées de la ligue 1 pour l'année 2022 - 2023
     # A modifier pour prendre en argument la ligue, et l'année ??   
  
@@ -27,20 +49,27 @@ def get_ligue1_journees():
     json.dump(journees, res_ligue1, indent = 6)
     res_ligue1.close()
 
-def clean_ligue1():
+def clean_journees():
     with open(json_path + 'raw/journees_ligue1_2022.json') as f:
         journee_22 = json.load(f)
-    journee_clean  = [{"date_match": journee["fixture"]["date"],
-                "num_journee" : journee["league"]["round"][-2:].strip(), 
-                "equipe_dom":journee["teams"]['home']['name'],
-                "equipe_vis": journee["teams"]['away']['name'],
-                "buts_dom": journee["goals"]["home"],
-                "buts_vis": journee["goals"]["away"]} for journee in journee_22["response"]]
+    journee_clean  = [{"model": "projet_AFPA.rencontre",
+                   "pk": pk+1,
+                   "fields": {"date_match": datetime.fromtimestamp(journee["fixture"]["timestamp"]).strftime("%Y-%m-%d"),
+                   "num_journee" : journee["league"]["round"][-2:].strip(), 
+                   "equipe_dom":journee["teams"]['home']['name'],
+                   "equipe_vis": journee["teams"]['away']['name'],
+                   "buts_dom": journee["goals"]["home"],
+                   "buts_vis": journee["goals"]["away"]}} for pk, journee in enumerate(journee_22["response"])]
     l1_22_clean = open(json_path + "clean/journees_ligue1_2022_clean.json", "w")
     json.dump(journee_clean, l1_22_clean, indent= 6)
     l1_22_clean.close()
     f.close()
+    
+def get_clubs():
+    with open(json_path + 'raw/journees_ligue1_2022.json') as f:
+        liste_club = json.load(f)
+    
    
 if __name__ == '__main__' :
-    get_ligue1_journees()
-    clean_ligue1()
+    get_journees()
+    clean_journees()
