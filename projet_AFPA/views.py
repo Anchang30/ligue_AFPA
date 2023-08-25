cb019b7c0eec2f2f297ce6dd843b93bd146e1b28
Lavilley <lavilleyvincent@gmail.com>
2023-08-25 16:37:45 +0200

    Front classement et rencontre, liens interpages OK

--- projet_AFPA/views.py
+++ projet_AFPA/views.py
@@ -7,8 +7,9 @@ from .get_data import mapping
 
 
 def home(request):
-    return HttpResponse("Coucou tout le monde, voici mon site web qui présente des \
-                        résultats divers et variés sur la ligue 1")
+    template = loader.get_template("home.html")
+    context = {}
+    return HttpResponse (template.render(context,request))
 
 # Affiche le classement pour la dernière journée. En choisissant une journée, on peut voir le classement pour celle-ci.
 def classement(request, date_journee = 0):
@@ -22,20 +23,23 @@ def classement(request, date_journee = 0):
     # liste des clubs
     liste_clubs = [club.nom for club in Club.objects.all()]
     for club in liste_clubs:
-        # récupère les rencontres où le club a joué, à domicile ou à l'extérieur
+    # récupère les rencontres où le club a joué, à domicile ou à l'extérieur
         rencontre_club = rencontre_list.filter(equipe_dom = mapping.get(club)) | rencontre_list.filter(equipe_vis = mapping.get(club))
         for rencontre in rencontre_club :
-            # pour chacun des match de ce club, on calcule le score et MAJ le classement
+    # pour chacun des match de ce club, on calcule le score et MAJ le classement
             get_score(club, classement, rencontre)
             classement[club]["joué"] +=1
             classement[club]["diff"] = classement[club]['buts_mis'] - classement[club]['buts_pris']
-    # sorted_ranking = dict(sorted(classement.items(), key=lambda x:(x[1]["nb_pts"], x[1]["diff"]),reverse = True))
-    sorted_ranking = ranking_test_prod = [(rank_+1, club, stats) for rank_, (club,stats)  in enumerate(sorted(classement.items(), key=lambda x:(x[1]["nb_pts"], x[1]["diff"]),reverse = True))]
+    # Trie la liste des clubs pour fournir le classement déjà ordonné
+    sorted_ranking = [(rank_+1, club, stats) for rank_, (club,stats)  in enumerate(sorted(classement.items(), key=lambda x:(x[1]["nb_pts"], x[1]["diff"]),reverse = True))]
+    # pour obtenir la liste des journées disponibles pour cette saison, pour l'affichage
+    liste_nb_journees = [journee for journee in range(1,(((Club.objects.count())-1)*2)+1)]
     template = loader.get_template("classement.html")
     context = {"classement" : sorted_ranking,
                "date_journee":date_journee,
                "rencontres" : rencontre_list,
-               "liste_clubs":liste_clubs}
+               "liste_clubs":liste_clubs,
+               "journees_liste": liste_nb_journees}
     return HttpResponse(template.render(context, request))
 
 
@@ -47,7 +51,12 @@ def rencontre(request, date_journee = 0):
     if not date_journee :
         date_journee = Rencontre.objects.all().last().num_journee   
     journee_list = sorted([rencontre for rencontre in Rencontre.objects.all().filter(num_journee = date_journee)], key = lambda r: r.date_match)     
+    logos = [(Club.objects.filter(nom=journee.equipe_dom).values("logo")[0]["logo"],Club.objects.filter(nom=journee.equipe_vis).values("logo")[0]["logo"]) for journee in journee_list]
+    journee_list_logo = zip(journee_list,logos)
+    # pour obtenir la liste des journées disponibles pour cette saison, pour l'affichage
+    liste_nb_journees = [journee for journee in range(1,(((Club.objects.count())-1)*2)+1)]
     template = loader.get_template("rencontre.html")
-    context = {"journee_list" : journee_list,
-               'date_journee': date_journee}
+    context = {"journee_list" : journee_list_logo,
+               "date_journee": date_journee,
+               "journees_liste": liste_nb_journees}
     return HttpResponse(template.render(context, request))
